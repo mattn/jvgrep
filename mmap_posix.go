50b8ec2bca6cafd9ba0e90ce9e29232c4cc07b8a
mattn <mattn.jp@gmail.com>
2013-07-19 10:47:16 +0900

    Add mmap

---
+++ mmap_posix.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"syscall"
+)
+
+type memfile []byte
+
+func OpenMemfile(filename string) (memfile, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	fs, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	fsize := fs.Size()
+	mem, err := syscall.Mmap(int(f.Fd()), 0, int(fi.Size()), syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
+		return nil, err
+	}
+	return memfile(mem), nil
+}
+
+func (mf memfile) Data() []byte {
+	return []byte(mf)
+}
+
+func (mf memfile) Close() {
+	syscall.Munmap([]byte(mf))
+}
---
+++ mmap_windows.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+type memfile uintptr
+
+func OpenMemfile(filename string) (memfile, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	fs, err := f.Stat()
+	if err != nil {
+		return 0, err
+	}
+	fsize := fs.Size()
+	fmap, err := syscall.CreateFileMapping(syscall.Handle(f.Fd()), nil, syscall.PAGE_READONLY, 0, uint32(fsize), nil)
+	if err != nil {
+		return 0, err
+	}
+	defer syscall.CloseHandle(fmap)
+	mem, err := syscall.MapViewOfFile(fmap, syscall.FILE_SHARE_READ, 0, 0, uintptr(fsize))
+	if err != nil {
+		return 0, err
+	}
+	return memfile(mem), nil
+}
+
+func (mf memfile) Data() []byte {
+	return *(*[]byte)(unsafe.Pointer(&mf))
+}
+
+func (mf memfile) Close() {
+	syscall.UnmapViewOfFile(uintptr(mf))
+}
