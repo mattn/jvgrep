5f9b39f31b636e691fe2a50b5ab52df18dc1c984
mattn <mattn.jp@gmail.com>
2014-05-20 22:26:15 +0900

    Upload

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+make
+VERSION=`./jvgrep -V`
 XC_ARCH=${XC_ARCH:-386 amd64}
 XC_OS=${XC_OS:-linux darwin windows}
 
@@ -8,3 +10,13 @@ gox \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
     -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"
+
+for ARCHIVE in ./pkg/*; do
+    ARCHIVE_NAME=$(basename ${ARCHIVE})
+    echo Uploading: ${ARCHIVE_NAME}
+    curl \
+        -T ${ARCHIVE} \
+        -u mattn:${BINTRAY_API_KEY} \
+        "https://api.bintray.com/content/mattn/jvgrep/jvgrep/${VERSION}/${ARCHIVE_NAME}"
+    echo
+done
